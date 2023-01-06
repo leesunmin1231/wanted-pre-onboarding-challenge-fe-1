5f85585cf07fcab66a4a1022e02a1e1c5bef9353
sunmin <polarlsm1231@gmail.com>
2023-01-07 02:48:34 +0900

    feat(fe): 메인 페이지 인증 확인

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import { Global, ThemeProvider } from '@emotion/react';
 import { RecoilRoot } from 'recoil';
 import styled from '@emotion/styled';
 import globalStyle from './styles/global';
-import Main from './pages/Main';
+import Todo from './pages/Todo';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import AppTheme from './styles/theme';
@@ -20,7 +20,7 @@ function App() {
             <Routes>
               <Route path="/auth/login" element={<Login />} />
               <Route path="/auth/signup" element={<SignUp />} />
-              <Route path="/" element={<Main />} />
+              <Route path="/" element={<Todo />} />
             </Routes>
           </Router>
         </AppStyle>
---
+++ client/src/pages/Todo.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loading from '../components/Loading';
+import useModal from '../hooks/useModal';
+
+export default function Todo() {
+  const [isLogined, setIsLogined] = useState(false);
+  const navigate = useNavigate();
+  const { setContent, closeModal } = useModal();
+  const modalButton = (name: string, url: string) => ({
+    name,
+    handler: () => {
+      navigate(url);
+      closeModal();
+    },
+  });
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      setContent('로그인이 필요한 페이지 입니다.\n로그인 하시겠습니까?', [
+        modalButton('회원가입', '/auth/signup'),
+        modalButton('로그인', '/auth/login'),
+      ]);
+    }
+    setIsLogined(true);
+  }, [setIsLogined]);
+  return <div>{isLogined ? <div>main</div> : <Loading />}</div>;
+}
